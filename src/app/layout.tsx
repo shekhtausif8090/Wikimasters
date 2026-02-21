2ae3656ac47d5a803cc86778438a3684a2fc9ded
Shekh <shekhtausif8090@gmail.com>
2026-02-21 22:15:06 +0530

    feat: add Vercel Analytics and Speed Insights to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/nav/nav-bar";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +32,8 @@ export default function RootLayout({
       >
         <NavBar />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
